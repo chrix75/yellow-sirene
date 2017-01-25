67e2de8f7a051e3d5955ce6b382a9b1572003013
Christian Sperandio <christian.sperandio@gmail.com>
2017-01-25 15:24:16 +0100

    Departements

--- src/main/resources/static/js/views/geo.js
+++ src/main/resources/static/js/views/geo.js
@@ -19,13 +19,16 @@ var Regions = function () {
         this.regions = [];
     }
 
-    Departement.prototype.selectionChanged = function(selected) {
-        this.selected = selected;
-        _.each(this.regions, function (r) { r.updateFilteredState(); });
+    Departement.prototype.selectionChanged = function () {
+        _.each(this.regions, function (r) {
+            r.updateFilteredState();
+        });
     };
 
     Region.prototype.updateFilteredState = function () {
-        var hasSelectedDpt = _.some(this.departements, function (d) { return d.selected; });
+        var hasSelectedDpt = _.some(this.departements, function (d) {
+            return d.selected;
+        });
         this.filtred = hasSelectedDpt;
     };
 
@@ -56,7 +59,7 @@ var Regions = function () {
         newRegion: function (name) {
             return new Region(name);
         },
-        newDepartement: function(name, code) {
+        newDepartement: function (name, code) {
             return new Departement(name, code);
         },
         defineRegion2016: function (newRegion, oldRegions) {
@@ -69,12 +72,14 @@ var Regions = function () {
 }();
 
 
-
 var geoView = new Vue({
     el: '#geo',
     data: {
-        visible: false,
-        action: 'Afficher',
+        regionsVisible: false,
+        departementsVisible: false,
+        showAllDepartements: true,
+        regionsAction: 'Afficher',
+        departementsAction: 'Afficher',
         regions: {
             alsace: Regions.newRegion('Alsace'),
             aquitaine: Regions.newRegion('Aquitaine'),
@@ -186,7 +191,7 @@ var geoView = new Vue({
             seineMaritime: Regions.newDepartement('Seine-Maritime', '76'),
             seineEtMarne: Regions.newDepartement('Seine-et-Marne', '77'),
             yvelines: Regions.newDepartement('Yvelines', '78'),
-            seuxSevres: Regions.newDepartement('Deux-Sèvres', '79'),
+            deuxSevres: Regions.newDepartement('Deux-Sèvres', '79'),
             somme: Regions.newDepartement('Somme', '80'),
             tarn: Regions.newDepartement('Tarn', '81'),
             tarnEtGaronne: Regions.newDepartement('Tarn-et-Garonne', '82'),
@@ -214,8 +219,94 @@ var geoView = new Vue({
         Regions.defineRegion2016(this.regions.occitanie, [this.regions.languedocRoussillon, this.regions.midiPyrenees]);
         Regions.defineRegion2016(this.regions.auvergneRhoneAlpes, [this.regions.auvergne, this.regions.rhoneAlpes]);
 
-        this.regions.auvergneRhoneAlpes.addDepartement(this.departements.ain);
-        this.regions.rhoneAlpes.addDepartement(this.departements.ain);
+        // Régions Auvergne Rhône Alpes
+        this.allocateDepartementsToRegion(this.regions.auvergneRhoneAlpes, [
+            this.departements.ain, this.departements.hauteSavoie, this.departements.savoie, this.departements.isere,
+            this.departements.drome, this.departements.rhone, this.departements.loire, this.departements.ardeche,
+            this.departements.hauteLoire, this.departements.allier, this.departements.puyDeDome, this.departements.cantal]);
+
+        this.allocateDepartementsToRegion(this.regions.rhoneAlpes, [
+            this.departements.ain, this.departements.hauteSavoie, this.departements.savoie, this.departements.isere,
+            this.departements.drome, this.departements.rhone, this.departements.loire, this.departements.ardeche]);
+
+        this.allocateDepartementsToRegion(this.regions.auvergne, [
+            this.departements.hauteLoire, this.departements.allier, this.departements.puyDeDome, this.departements.cantal]);
+
+        // Régions Hauts de France
+        this.allocateDepartementsToRegion(this.regions.picardie, [this.departements.aisne, this.departements.oise, this.departements.somme]);
+        this.allocateDepartementsToRegion(this.regions.nordPasDeCalais, [this.departements.nord, this.departements.pasDeCalais]);
+        this.allocateDepartementsToRegion(this.regions.hautsDeFrance, [this.departements.nord, this.departements.pasDeCalais,
+            this.departements.aisne, this.departements.oise, this.departements.somme]);
+
+        // Régions Nouvelle Aquitaine
+        this.allocateDepartementsToRegion(this.regions.aquitaine, [this.departements.dordogne, this.departements.gironde,
+            this.departements.landes, this.departements.lotEtGaronne, this.departements.pyreneesAtlantiques]);
+        this.allocateDepartementsToRegion(this.regions.poitouCharentes, [this.departements.charente, this.departements.charenteMaritime,
+            this.departements.vienne, this.departements.deuxSevres]);
+        this.allocateDepartementsToRegion(this.regions.limousin, [this.departements.correze, this.departements.creuse,
+            this.departements.hauteVienne]);
+        this.allocateDepartementsToRegion(this.regions.nouvelleAquitaine, [
+            this.departements.dordogne, this.departements.gironde, this.departements.landes, this.departements.lotEtGaronne, this.departements.pyreneesAtlantiques,
+            this.departements.charente, this.departements.charenteMaritime, this.departements.vienne, this.departements.deuxSevres,
+            this.departements.correze, this.departements.creuse, this.departements.hauteVienne]);
+
+        // Régions Normandie
+        this.allocateDepartementsToRegion(this.regions.hauteNormandie, [this.departements.seineMaritime, this.departements.eure]);
+        this.allocateDepartementsToRegion(this.regions.basseNormandie, [this.departements.calvados, this.departements.manche, this.departements.orne]);
+        this.allocateDepartementsToRegion(this.regions.normandie, [this.departements.seineMaritime, this.departements.eure,
+            this.departements.calvados, this.departements.manche, this.departements.orne]);
+
+        // Régions Bourgogne Franche Comté
+        this.allocateDepartementsToRegion(this.regions.francheComte, [this.departements.doubs, this.departements.jura,
+            this.departements.hauteSaone, this.departements.territoireDeBelfort]);
+        this.allocateDepartementsToRegion(this.regions.bourgogne, [this.departements.coteOr, this.departements.nievre,
+            this.departements.saoneEtLoire, this.departements.yonne]);
+
+        // Bretagne
+        this.allocateDepartementsToRegion(this.regions.bretagne, [this.departements.cotesArmor, this.departements.finistere,
+            this.departements.illeEtVilaine, this.departements.morbihan]);
+
+        // Centre
+        this.allocateDepartementsToRegion(this.regions.centreValDeLoire, [this.departements.cher, this.departements.eureEtLoir,
+            this.departements.indre, this.departements.indreEtLoire, this.departements.loirEtCher, this.departements.loiret]);
+
+        // Régions Grand Est
+        this.allocateDepartementsToRegion(this.regions.alsace, [this.departements.basRhin, this.departements.hautRhin, this.departements.territoireDeBelfort]);
+        this.allocateDepartementsToRegion(this.regions.lorraine, [this.departements.meurtheEtMoselle, this.departements.meuse, this.departements.moselle, this.departements.vosges]);
+        this.allocateDepartementsToRegion(this.regions.champagneArdennes, [this.departements.ardennes, this.departements.aube, this.departements.marne, this.departements.hauteMarne]);
+        this.allocateDepartementsToRegion(this.regions.grandEst, [
+            this.departements.basRhin, this.departements.hautRhin, this.departements.territoireDeBelfort,
+            this.departements.meurtheEtMoselle, this.departements.meuse, this.departements.moselle, this.departements.vosges,
+            this.departements.ardennes, this.departements.aube, this.departements.marne, this.departements.hauteMarne
+        ]);
+
+        // IDF
+        this.allocateDepartementsToRegion(this.regions.idf, [this.departements.paris, this.departements.seineEtMarne,
+            this.departements.yvelines, this.departements.essonne, this.departements.hautsDeSeine, this.departements.seineSaintDenis,
+            this.departements.valDeMarne, this.departements.valOise]);
+
+        // Régions Occitanie
+        this.allocateDepartementsToRegion(this.regions.languedocRoussillon, [this.departements.aude, this.departements.gard,
+            this.departements.herault, this.departements.lozere, this.departements.pyreneesOrientales]);
+        this.allocateDepartementsToRegion(this.regions.midiPyrenees, [this.departements.ariege, this.departements.aveyron,
+            this.departements.hauteGaronne, this.departements.gers, this.departements.lot, this.departements.hautesPyrenees,
+            this.departements.tarn, this.departements.tarnEtGaronne]);
+        this.allocateDepartementsToRegion(this.regions.occitanie, [this.departements.aude, this.departements.gard,
+            this.departements.herault, this.departements.lozere, this.departements.pyreneesOrientales,
+            this.departements.ariege, this.departements.aveyron,
+            this.departements.hauteGaronne, this.departements.gers, this.departements.lot, this.departements.hautesPyrenees,
+            this.departements.tarn, this.departements.tarnEtGaronne]);
+
+        // Pays de la loire
+        this.allocateDepartementsToRegion(this.regions.paysDeLaLoire, [this.departements.loireAtlantique, this.departements.maineEtLoire,
+            this.departements.mayenne, this.departements.sarthe, this.departements.vendee]);
+
+        // paca
+        this.allocateDepartementsToRegion(this.regions.paca, [this.departements.alpesDeHautesProvence, this.departements.hautesAlpes,
+            this.departements.alpesMaritimes, this.departements.bouchesDuRhone, this.departements.var, this.departements.vaucluse]);
+
+        // corse
+        this.allocateDepartementsToRegion(this.regions.corse, [this.departements.corseSud, this.departements.hauteCorse]);
 
     },
     computed: {
@@ -249,16 +340,208 @@ var geoView = new Vue({
                 this.regions.poitouCharentes.selected ||
                 this.regions.paca.selected ||
                 this.regions.rhoneAlpes.selected;
+        },
+        departementsFromSelectedRegions: function () {
+            var selectedRegions = this.findSelectedRegions();
+            var dpts = _.sortBy(_.flattenDeep(_.map(selectedRegions, function (r) {
+                return r.departements;
+            })), function (o) {
+                return o.name;
+            });
+
+            return _.uniqWith(dpts, function (d1, d2) {
+                return d1.name == d2.name;
+            });
+        },
+        departementsGroupA: function () {
+            if (this.showAllDepartements) {
+                return [this.departements.ain,
+                    this.departements.aisne,
+                    this.departements.allier,
+                    this.departements.alpesDeHautesProvence,
+                    this.departements.hautesAlpes,
+                    this.departements.alpesMaritimes,
+                    this.departements.ardeche,
+                    this.departements.ardennes,
+                    this.departements.ariege,
+                    this.departements.aube,
+                    this.departements.aude,
+                    this.departements.aveyron,
+                    this.departements.bouchesDuRhone,
+                    this.departements.calvados,
+                    this.departements.cantal,
+                    this.departements.charente,
+                    this.departements.charenteMaritime,
+                    this.departements.cher,
+                    this.departements.correze,
+                    this.departements.corseSud,
+                    this.departements.hauteCorse,
+                    this.departements.coteOr,
+                    this.departements.cotesArmor,
+                    this.departements.creuse,
+                    this.departements.dordogne,
+                    this.departements.doubs,
+                    this.departements.drome,
+                    this.departements.eure,
+                    this.departements.eureEtLoir,
+                    this.departements.finistere,
+                    this.departements.gard,
+                    this.departements.hauteGaronne];
+            } else {
+                var blockSize = Math.ceil(this.departementsFromSelectedRegions.length / 3);
+
+                return _.take(this.departementsFromSelectedRegions, blockSize);
+            }
+        },
+        departementsGroupB: function () {
+            if (this.showAllDepartements) {
+                return [this.departements.gers,
+                    this.departements.gironde,
+                    this.departements.herault,
+                    this.departements.illeEtVilaine,
+                    this.departements.indre,
+                    this.departements.indreEtLoire,
+                    this.departements.isere,
+                    this.departements.jura,
+                    this.departements.landes,
+                    this.departements.loirEtCher,
+                    this.departements.loire,
+                    this.departements.hauteLoire,
+                    this.departements.loireAtlantique,
+                    this.departements.loiret,
+                    this.departements.lot,
+                    this.departements.lotEtGaronne,
+                    this.departements.lozere,
+                    this.departements.maineEtLoire,
+                    this.departements.manche,
+                    this.departements.marne,
+                    this.departements.hauteMarne,
+                    this.departements.mayenne,
+                    this.departements.meurtheEtMoselle,
+                    this.departements.meuse,
+                    this.departements.morbihan,
+                    this.departements.moselle,
+                    this.departements.nievre,
+                    this.departements.nord,
+                    this.departements.oise,
+                    this.departements.orne,
+                    this.departements.pasDeCalais,
+                    this.departements.puyDeDome];
+            } else {
+                var blockSize = Math.ceil(this.departementsFromSelectedRegions.length / 3);
+
+                return _.take(_.drop(this.departementsFromSelectedRegions, blockSize), blockSize);
+            }
+        },
+        departementsGroupC: function () {
+            if (this.showAllDepartements) {
+                return [this.departements.pyreneesAtlantiques,
+                    this.departements.hautesPyrenees,
+                    this.departements.pyreneesOrientales,
+                    this.departements.basRhin,
+                    this.departements.hautRhin,
+                    this.departements.rhone,
+                    this.departements.hauteSaone,
+                    this.departements.saoneEtLoire,
+                    this.departements.sarthe,
+                    this.departements.savoie,
+                    this.departements.hauteSavoie,
+                    this.departements.paris,
+                    this.departements.seineMaritime,
+                    this.departements.seineEtMarne,
+                    this.departements.yvelines,
+                    this.departements.deuxSevres,
+                    this.departements.somme,
+                    this.departements.tarn,
+                    this.departements.tarnEtGaronne,
+                    this.departements.var,
+                    this.departements.vaucluse,
+                    this.departements.vendee,
+                    this.departements.vienne,
+                    this.departements.hauteVienne,
+                    this.departements.vosges,
+                    this.departements.yonne,
+                    this.departements.territoireDeBelfort,
+                    this.departements.essonne,
+                    this.departements.hautsDeSeine,
+                    this.departements.seineSaintDenis,
+                    this.departements.valDeMarne,
+                    this.departements.valOise];
+            } else {
+                var blockSize = Math.ceil(this.departementsFromSelectedRegions.length / 3);
+
+                return _.drop(this.departementsFromSelectedRegions, 2 * blockSize);
+            }
         }
     },
     methods: {
-        toggleDisplay: function () {
-            this.visible = !this.visible;
+        allocateDepartementsToRegion: function (reg, dpts) {
+            _.each(dpts, function (d) {
+                reg.addDepartement(d);
+            })
+        },
+        findSelectedRegions: function () {
+            var allRegions = [this.regions.alsace,
+                this.regions.aquitaine,
+                this.regions.auvergne,
+                this.regions.auvergneRhoneAlpes,
+                this.regions.basseNormandie,
+                this.regions.bourgogne,
+                this.regions.bougogneFrancheComte,
+                this.regions.francheComte,
+                this.regions.bretagne,
+                this.regions.centreValDeLoire,
+                this.regions.champagneArdennes,
+                this.regions.corse,
+                this.regions.grandEst,
+                this.regions.hauteNormandie,
+                this.regions.hautsDeFrance,
+                this.regions.idf,
+                this.regions.languedocRoussillon,
+                this.regions.limousin,
+                this.regions.lorraine,
+                this.regions.midiPyrenees,
+                this.regions.nordPasDeCalais,
+                this.regions.normandie,
+                this.regions.nouvelleAquitaine,
+                this.regions.occitanie,
+                this.regions.paysDeLaLoire,
+                this.regions.picardie,
+                this.regions.poitouCharentes,
+                this.regions.paca,
+                this.regions.rhoneAlpes];
 
-            if (this.visible) {
-                this.action = 'Masquer';
+            return _.filter(allRegions, function (r) {
+                return r.selected;
+            });
+        },
+        toggleDisplayRegions: function () {
+            this.regionsVisible = !this.regionsVisible;
+
+            if (this.regionsVisible) {
+                this.regionsAction = 'Masquer';
             } else {
-                this.action = 'Afficher';
+                this.regionsAction = 'Afficher';
+            }
+        },
+        toggleDisplayDepartements: function () {
+            this.departementsVisible = !this.departementsVisible;
+
+            if (this.departementsVisible) {
+                this.departementsAction = 'Masquer';
+                this.showAllDepartements = true;
+            } else {
+                this.departementsAction = 'Afficher';
+            }
+        },
+        toggleDisplayFiltredDepartements: function () {
+            this.departementsVisible = !this.departementsVisible;
+
+            if (this.departementsVisible) {
+                this.departementsAction = 'Masquer';
+                this.showAllDepartements = false;
+            } else {
+                this.departementsAction = 'Afficher';
             }
         },
         toggleRegion: function (reg) {
